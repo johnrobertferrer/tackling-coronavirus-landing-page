896b25a09d023ca68c33aeaf0445b65b65d7b980
John Ferrer <johnrobert.ferrer.nelsoft@gmail.com>
2020-03-30 02:48:42 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,8 +28,8 @@
 			</div>
 		</section>
 
-		<section class="section c-s-full c-mb-5" id="section-3">
-			<div class="container c-pb-6">
+		<section class="section c-s-full c-mb-2" id="section-3">
+			<div class="container c-pb-3">
 				<h1 class="title is-1 has-text-primary c-mt-3">COVID-19 epidemic math models and some insights focusing on the Philippines</h1>
 				<h2 class="subtitle c-mt-7">Published online: 22 March 2020</h2>
 				<h2 class="subtitle">Updated: 26 March 2020</h2>
@@ -38,7 +38,7 @@
 		</section>
 
 		<!-- FOOTER -->
-		<div class="c-pt-5 c-pb-5"></div>
+		<div class="c-pt-4 c-pb-4"></div>
 
 		<footer class="footer has-background-primary">
 			<div class="content has-text-centered">
